0f44ed266be92beb5d5859bbc97ce6f4e008568d
Maryanne-Carvalho <mary.lima9@hotmail.com>
2024-08-02 19:37:45 -0300

    Atividades parada 42

---
+++ atv05.py
@@ -0,0 +1,8 @@
+#5) Faça um programa que calcule a área de um quadrado/retângulo.
+
+largura = float(input("Digite a largura: "))
+altura = float(input("Digite a altura: "))
+
+area = largura * altura
+
+print(f"A área do quadrado/retângulo é: {area:.2f}")
