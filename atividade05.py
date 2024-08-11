6d48051180b539dc268beddc15a7813c7d19431d
Maryanne-Carvalho <mary.lima9@hotmail.com>
2024-08-11 15:04:07 -0300

    Atividades parada 44

---
+++ atividade05.py
@@ -0,0 +1,6 @@
+#5) Faça um programa que recebe uma string digitada pelo usuário, o programa deve exibir todas as letras que compõem a palavra digitada.  
+
+palavra = input("Digite uma palavra: ")
+
+for letra in palavra:
+    print(letra)
